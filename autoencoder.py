7191f655c0c016461386e4c072e914023d814340
vladim0105 <vladim0105@gmail.com>
2021-09-16 16:47:59 +0200

    Improved AE

--- autoencoder.py
+++ autoencoder.py
@@ -1,5 +1,9 @@
 import glob
+import json
+import os
+import pickle
 import typing
+from pathlib import Path
 
 import numpy as np
 import progressbar
@@ -20,7 +24,7 @@ class ImageReconstructionDataset(Dataset):
         self.device = device
 
     def __getitem__(self, index) -> T_co:
-        pil_img = Image.open(self.files[index]).convert('RGB')
+        pil_img = Image.open(self.files[index]).convert('L')
         img = self.transform(pil_img).to(self.device)
         return img, img
 
@@ -31,33 +35,35 @@ class ImageReconstructionDataset(Dataset):
 class Encoder(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
-        self.base = torchvision.models.resnet.resnet18(pretrained=True)
-        self.base.fc = nn.Linear(in_features=512, out_features=latent_dim)
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv2d(in_channels=1, stride=(2, 2), out_channels=3, kernel_size=(5, 5), padding=2)
+        self.conv2 = nn.Conv2d(in_channels=3, stride=(2, 2), out_channels=6, kernel_size=(3, 3), padding=1)
+        self.conv3 = nn.Conv2d(in_channels=6, stride=(2, 2), out_channels=12, kernel_size=(3, 3), padding=1)
 
     def forward(self, x):
-        return self.base(x)
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        return x
 
 
 class Decoder(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
         self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(in_features=latent_dim, out_features=256)
-        self.upconv1 = nn.ConvTranspose2d(in_channels=1, stride=(2, 2), out_channels=3, kernel_size=(2, 2))
-        self.upconv2 = nn.ConvTranspose2d(in_channels=3, stride=(2, 2), out_channels=3, kernel_size=(2, 2))
-        self.upconv3 = nn.ConvTranspose2d(in_channels=3, stride=(2, 2), out_channels=3, kernel_size=(2, 2))
-        self.upconv4 = nn.ConvTranspose2d(in_channels=3, stride=(2, 2), out_channels=3, kernel_size=(2, 2))
+        self.upconv1 = nn.ConvTranspose2d(in_channels=12, stride=(2, 2), out_channels=6, kernel_size=(2, 2))
+        self.upconv2 = nn.ConvTranspose2d(in_channels=6, stride=(2, 2), out_channels=3, kernel_size=(2, 2))
+        self.upconv3 = nn.ConvTranspose2d(in_channels=3, stride=(2, 2), out_channels=1, kernel_size=(2, 2))
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = x.reshape(-1, 1, 16, 16)
         x = self.upconv1(x)
         x = self.relu(x)
         x = self.upconv2(x)
         x = self.relu(x)
         x = self.upconv3(x)
-        x = self.relu(x)
-        x = self.upconv4(x)
         x = self.relu(x)
         return x
 
@@ -81,12 +87,20 @@ class Params(TypedDict):
     num_epochs: int
     model: nn.Module
     optimizer: torch.optim.Optimizer
+    scheduler: object
     loss_func: typing.Callable[[torch.Tensor, torch.Tensor], torch.Tensor]
     train_loader: torch.utils.data.DataLoader
     device: torch.device
     name: str
 
+
 def train(params: Params):
+    Path(f"./{params['name']}").mkdir(exist_ok=True)
+    Path(f"./{params['name']}/images").mkdir(exist_ok=True)
+    Path(f"./{params['name']}/checkpoints").mkdir(exist_ok=True)
+    with open(f"{params['name']}/params.json", "w") as file:
+        file.write(json.dumps(params, default=lambda o: str(o)))  # use `json.loads` to do the reverse
+
     for epoch in range(params["num_epochs"]):
         losses = []
         with progressbar.ProgressBar(max_value=len(params["train_loader"])) as bar:
@@ -95,32 +109,44 @@ def train(params: Params):
 
                 params["optimizer"].zero_grad()
 
-                outputs, _ = params["model"](inputs)
+                outputs, latent = params["model"](inputs)
                 loss = params["loss_func"](outputs, targets)
                 loss.backward()
                 params["optimizer"].step()
                 losses.append(loss.item())
-                if batch_idx == 0 and epoch % 5 == 0:
+                if batch_idx == 0 and epoch % (params["num_epochs"] / 10) == 0:
                     torchvision.utils.save_image(outputs, f"{params['name']}/images/out_{epoch}.png")
-                    torch.save(params["model"].state_dict(), f"{params['name']}/checkpoints/checkpoint_{epoch}.pt")
+                    torchvision.utils.save_image(latent[0, 0, :, :], f"{params['name']}/images/latent_{epoch}.png")
+
                 bar.update(bar.value + 1)
-        print(f"Epoch: {epoch}, Loss: {np.average(losses)}")
+        loss_metric = np.average(losses)
+        print(f"Epoch: {epoch}, Loss: {loss_metric}")
+        # TODO Validation
+        scheduler.step(loss_metric)
+        if epoch % int(params["num_epochs"] / 5) == 0:
+            torch.save(params["model"].state_dict(), f"{params['name']}/checkpoints/checkpoint_{epoch}.pt")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", type=str, help="Name of the model, used for storing files", required=True)
+    parser.add_argument("-d", "--data", type=str, help="Path to folder containing data", required=True)
+    parser.add_argument("-e", "--epochs", type=int, help="Number of epochs", default=50)
+    parser.add_argument("-b", "--batch", type=int, help="Batch size", default=20)
+    parser.add_argument("--lr", type=float, help="Learning rate", default=0.01)
     args = parser.parse_args()
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    dataset = ImageReconstructionDataset(".", "png", device)
-    train_loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
-    model = AutoEncoder(latent_dim=50)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
+    dataset = ImageReconstructionDataset(args.data, "png", device)
+    train_loader = DataLoader(dataset=dataset, batch_size=args.batch, shuffle=True)
+    model = AutoEncoder(latent_dim=0)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True)
     params: Params = {
-        "num_epochs": 300,
+        "num_epochs": args.epochs,
         "model": model.to(device),
         "optimizer": optimizer,
+        "scheduler": scheduler,
         "loss_func": nn.MSELoss().to(device),
         "train_loader": train_loader,
         "device": device,
