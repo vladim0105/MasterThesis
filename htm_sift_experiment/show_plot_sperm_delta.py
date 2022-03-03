93f3e2f4ff8e0c7bbefae471bc7e175e23b010fc
vladim0105 <vladim0105@gmail.com>
2022-03-03 18:20:24 +0100

    Code

---
+++ htm_sift_experiment/show_plot_sperm_delta.py
@@ -0,0 +1,50 @@
+import argparse
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+import utils
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("file", type=str)
+
+    args =argparser.parse_args()
+
+    data = pickle.load(open(args.file,"rb"))
+    offset = 200
+    anoms = data["anom_scores"][offset:]
+    l1_scores = data["l1_scores"][offset:]
+
+    anoms = utils.trailing_average(anoms, 100)
+    l1_scores = utils.trailing_average(l1_scores, 100)
+
+    #anoms = np.diff(anoms, n=1)
+    #l1_scores = np.diff(l1_scores, n=1)
+
+
+    fig, ax = plt.subplots()
+
+    # Plot linear sequence, and set tick labels to the same color
+    anom_score_plot, = ax.plot(anoms, color='blue', label="Anomaly Score", alpha=0.75)
+    ax.tick_params(axis='y', labelcolor='blue')
+    ax.set_ylabel("Anomaly Score")
+    ax.set_xlabel("Frames")
+    # Generate a new Axes instance, on the twin-X axes (same position)
+    ax2 = ax.twinx()
+
+    # Plot exponential sequence, set scale to logarithmic and change tick color
+    l1_plot, = ax2.plot(l1_scores, color='green', label="L1 Error", alpha=0.75)
+    ax2.tick_params(axis='y', labelcolor='green')
+    ax2.set_ylabel("L1 Error")
+
+    anom_marker_plots = None
+    if "anom_markers" in data:
+        anom_marker_plots = []
+        anom_markers = data["anom_markers"]
+        for anom_marker in anom_markers:
+            plot = plt.axvline(anom_marker-offset, c="red", alpha=0.3)
+            anom_marker_plots.append(plot)
+
+    plt.legend([anom_score_plot, l1_plot, tuple(anom_marker_plots) if "anom_markers" in data else None], ["Grid HTM", "L1 Error", "Segments"])
+    plt.show()
