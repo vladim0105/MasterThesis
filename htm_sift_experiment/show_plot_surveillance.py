97c961694eb0624775116f614e25777d121cee2a
vladim0105 <vladim0105@gmail.com>
2022-03-11 17:28:49 +0100

    Code

--- htm_sift_experiment/show_plot_surveillance.py
+++ htm_sift_experiment/show_plot_surveillance.py
@@ -2,8 +2,14 @@ import argparse
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
-
+import model
 import utils
+plt.rcParams["figure.figsize"] = (10, 4.8)
+
+def test(anoms, axis=None):
+    return np.sum(anoms, axis=axis)
+
+
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
@@ -14,9 +20,15 @@ if __name__ == "__main__":
     data = pickle.load(open(args.file,"rb"))
     offset = 200
     moving_avg = 100
-    anoms = data["anom_scores"][offset:]
-    x_vals = data["x_vals"][moving_avg+1:]
 
+    anoms = np.array(data["raw_anoms"])
+    anoms = np.mean(anoms, axis=(1,2))[offset:]
+    #anoms[anoms < 0.025] = 0
+    #anoms = model.grid_mean_aggr_func(anoms, (1,2))[offset:]
+    #anoms = test(anoms, (1,2))[offset:]*model.grid_mean_aggr_func(anoms, (1,2))[offset:]
+    x_vals = np.array(data["x_vals"])[offset-moving_avg+1:]
+    frame_freeze = data["frame_freeze"]-offset
+    #anoms = data["anom_scores"][offset:]
 
     fig, ax = plt.subplots()
 
@@ -26,6 +38,8 @@ if __name__ == "__main__":
     ax.set_ylabel("Anomaly Score")
     ax.set_xlabel("Frames")
 
+    frame_freeze_plot = plt.axvline(frame_freeze, c="green", alpha=0.3)
+
 
     anom_marker_plots = []
     anom_markers = data["anom_markers"]
@@ -33,5 +47,5 @@ if __name__ == "__main__":
         plot = plt.axvline(anom_marker-offset, c="red", alpha=0.3)
         anom_marker_plots.append(plot)
 
-    plt.legend([anom_score_plot, tuple(anom_marker_plots) if "anom_markers" in data else None], ["Grid HTM", "Anomalies"])
+    plt.legend([anom_score_plot, tuple(anom_marker_plots), frame_freeze_plot], ["Grid HTM", "Segments", "Frame Freeze"])
     plt.show()
